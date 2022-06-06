c37f8240e5c9ec3c21161f4aa58b9aa62820a086
Naomi Winchurch <naomi.winchurch@iterativeit.com>
2022-06-06 13:24:31 +0100

    Prior to fixing build script

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,10 +1,123 @@
 // Importing module
 // import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
-// console.log('Importing module');
+console.log('Importing module');
 // addToCart('bread', 5);
 // console.log(price, tq);
 // console.log('Importing module');
 
-import * as ShoppingCart from './shoppingCart.js';
-ShoppingCart.addToCart('bread', 5);
-console.log(ShoppingCart.totalPrice);
+// import * as ShoppingCart from './shoppingCart.js';
+// ShoppingCart.addToCart('bread', 5);
+// console.log(ShoppingCart.totalPrice);
+// import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// console.log(price);
+
+import add, { cart } from './shoppingCart.js';
+add('pizza', 2);
+add('bread', 25);
+add('apples', 4);
+
+console.log(cart);
+/*
+// Top level await i.e. await used outside of async function only works when used inside modules
+
+// console.log('Start fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('Something');
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  console.log(data);
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+// To get data out of an async function, you have to use the then method to consume the promise, otherwise you just get a pending promise result
+const lastPost = getLastPost();
+console.log(lastPost);
+
+//Not very clean
+// lastPost.then(last => console.log(last));
+
+// You can also use top level await to get data out of an async function without using the then method
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
+*/
+/*
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+    );
+  };
+
+  const orderStock = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2);
+*/
+
+// Common JS particularly important module system in Javascript
+// Export
+/*export.addTocart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+    );
+  };
+
+  // Import
+  const { addTocart } = require('./shoppingCart.js')
+  */
+
+// Introduction to NPM
+// import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+import cloneDeep from 'lodash-es';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+state.user.loggedIn = false;
+console.log(stateClone);
+
+console.log(stateDeepClone);
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+class Person {
+  greeting = 'Hey';
+  constructor(name) {
+    this.name = name;
+    console.log(`${this.greeting}, ${this.name}`);
+  }
+}
+const jonas = new Person('Jonas');
